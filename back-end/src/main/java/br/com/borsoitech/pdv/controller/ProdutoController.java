e972192f36459d116de498865e8786c8cab57d04
HENRIQUE BORSOI LOPES <00240508@acad.tol.unipar.br>
2024-05-27 22:17:01 -0300

    feat(produto): getAllPaged

--- back-end/src/main/java/br/com/borsoitech/pdv/controller/ProdutoController.java
+++ back-end/src/main/java/br/com/borsoitech/pdv/controller/ProdutoController.java
@@ -1,10 +1,27 @@
 package br.com.borsoitech.pdv.controller;
 
+import br.com.borsoitech.pdv.entity.Cliente;
+import br.com.borsoitech.pdv.entity.Produto;
+import br.com.borsoitech.pdv.service.ProdutoServico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-
+    
+    private ProdutoServico produtoService;
+    
+    public ProdutoController(ProdutoServico produtoService) {
+        this.produtoService = produtoService;
+    }
+    
+    @GetMapping
+    public ResponseEntity<Page<Produto>> getAllPaged(Pageable pageable) {
+        return ResponseEntity.ok().body(produtoService.acharTodosPaginado(pageable));
+    }
 }
